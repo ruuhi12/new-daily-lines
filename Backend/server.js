782e7821eb08209e9911567c9a105f9aa0a4c79f
sameera <ruuhi676@gmail.com>
2026-01-03 15:34:45 +0530

    auth

--- Backend/server.js
+++ Backend/server.js
@@ -8,9 +8,14 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: "https://dailylines-cad.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+app.options("*", cors());
 
 
 app.use(express.json());
