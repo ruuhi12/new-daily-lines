1f2740aa10d09de7821e4d81c348d048f85c4707
sameera <ruuhi676@gmail.com>
2026-01-03 15:25:52 +0530

    Enable CORS for frontend

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,15 @@ app.use("/api/auth", authRoutes);
 
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: "https://dailylines-cad.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
+
+app.use(express.json());
+
 
 mongoose.connect("mongodb+srv://admin:L4rxvmJmG42QohGq@cluster0.ikekv48.mongodb.net/?appName=Cluster0")
   .then(() => console.log("MongoDB Connected"))
